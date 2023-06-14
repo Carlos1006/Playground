c987216d416a8ac025b36e218d39d01a9b839ede
Naker21 <cdgzz19@gmail.com>
2023-06-14 00:41:57 -0600

    Se preparan estilos basicos
    Sombras
    Tamaños
    Variables de scss
    Colores

--- src/components/daynightToggle.tsx
+++ src/components/daynightToggle.tsx
@@ -1,10 +1,20 @@
 import css from '../styles/daynightToggle.module.scss';
 
+const Cloud = () => {
+    return <>
+        <div className={css.cloud}>
+
+        </div>
+    </>
+}
+
 const DayNightToggle = () => {
 
     return <>
         <div className={css.toggle}>
-
+            <div className={css.circle}>
+                <div className={css.center}/>
+            </div>
         </div>
     </>
 }
