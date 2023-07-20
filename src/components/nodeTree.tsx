4b63a28992413e71c1ed464a63b32d06eeab6cfe
Naker21 <cdgzz19@gmail.com>
2023-07-20 10:54:17 -0600

    Se crea node tree
    Se espera actualizacion de estilos

--- src/components/nodeTree.tsx
+++ src/components/nodeTree.tsx
@@ -1,81 +1,291 @@
-import React, { FC, ReactNode, createContext, forwardRef, useContext, useEffect, useState } from "react";
-import css from './../styles/nodeTree.module.scss';
+import {
+  FC,
+  createContext,
+  useContext,
+  useEffect,
+  CSSProperties,
+  useState,
+  useRef,
+  useMemo,
+} from "react";
+import css from "./../styles/nodeTree.module.scss";
+const nodeWidth: number = css.nodeWidth as unknown as number;
+const nodeHeight: number = css.nodeHeight as unknown as number;
 
-interface NodeProps {
-    name: string;
-    level: number;
-    nodes: NodeProps[];
+// ? INTERFACES
+interface NodeComponentProps {
+  name: string;
+  level?: number;
+  nodes: NodeComponentProps[];
+  defaultOpen?: boolean;
+  style?: CSSProperties;
+  propagatedOpen?: boolean | null;
+  parentOffset?: Offset;
+  siblings?: number;
+  index?: number;
+  alignment?: "horizontal" | "vertical";
 }
 
-interface NodeLevelProps {
-    level: number;
-    children: JSX.Element[] | JSX.Element;
+interface RowComponentProps {
+  level: number;
+  nodes: NodeComponentProps[];
+  open: boolean;
+  parentOffset?: Offset;
+  direction?: "horizontal" | "vertical";
 }
 
-const NodeTree:FC = () => {
-    
-    const nodeTree: NodeProps[] = [
-        {name:'node1', nodes:[
-            {name:'node4', nodes:[], level: -1},
-            {name:'node5', nodes:[], level: -1},
-            {name:'node6', nodes:[], level: -1},
-            {name:'node7', nodes:[], level: -1}
-        ], level: -1},
-        {name:'node2', nodes:[], level: -1},
-        {name:'node3', nodes:[
-            {name:'node8', nodes:[], level: -1},
-            {name:'node9', nodes:[], level: -1},
-            {name:'node10', nodes:[], level: -1},
-            {name:'node11', nodes:[], level: -1}
-        ], level: -1},
-    ]
+interface Offset {
+  left: number;
+  top: number;
+}
 
-    useEffect(() => {
-        // get all elements with data-level attribute
-        const elements = document.querySelectorAll('[data-level]');
-        // loop through all elements
-        console.log(elements);
-        elements.forEach((element: HTMLElement): void => {
-            const level = element.getAttribute('data-level');
-            const width = element.offsetWidth;
-            console.log(level, width);
-        });
-    });
+interface NodeTreeContextProps {
+  triggerOffset: number;
+  setTriggerOffset: (value: number) => void;
+}
 
-    return <>
+// ? CONTEXT
+const NodeTreeContext = createContext<NodeTreeContextProps>({
+  triggerOffset: 0,
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  setTriggerOffset: () => {},
+});
+
+// ? UTILS
+const getRandomColor = (): string => {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+// ? CONSTANTS
+const ForceVerticalMode: string[] = ["node2"];
+
+// ? COMPONENTS
+
+const NodeTree: FC = () => {
+  const [triggerOffset, setTriggerOffset] = useState<number>(0);
+  const nodeTree: NodeComponentProps[] = [
+    {
+      name: "node1",
+      nodes: [
+        { name: "node4", nodes: [] },
+        { name: "node5", nodes: [] },
+        { name: "node6", nodes: [] },
+        { name: "node7", nodes: [] },
+      ],
+    },
+    {
+      name: "node2",
+      nodes: [
+        { name: "node15", nodes: [] },
+        { name: "node16", nodes: [] },
+        { name: "node17", nodes: [] },
+      ],
+    },
+    {
+      name: "node3",
+      nodes: [
+        { name: "node8", nodes: [] },
+        { name: "node9", nodes: [] },
+        {
+          name: "node10",
+          nodes: [
+            { name: "node12", nodes: [] },
+            { name: "node13", nodes: [] },
+            { name: "node14", nodes: [] },
+          ],
+        },
+        { name: "node11", nodes: [] },
+      ],
+    },
+  ];
+
+  const contextValue: NodeTreeContextProps = {
+    triggerOffset,
+    setTriggerOffset,
+  };
+
+  return (
+    <>
+      <NodeTreeContext.Provider value={contextValue}>
         <div className={css.nodeTree}>
-            <Node level={0} name={'node0'} nodes={nodeTree} />
+          <Node
+            name="root"
+            level={0}
+            nodes={nodeTree}
+            defaultOpen={false}
+            siblings={nodeTree.length}
+            index={0}
+          />
         </div>
+      </NodeTreeContext.Provider>
     </>
-}
+  );
+};
 
-const NodeLevel:FC<NodeLevelProps> = ({ level, children }: NodeLevelProps) => {
-    const childrenLength = Array.isArray(children) ? children.length : 1;
+const Node: FC<NodeComponentProps> = ({
+  name,
+  level,
+  nodes,
+  style = {},
+  defaultOpen = false,
+  propagatedOpen = null,
+  parentOffset = { left: 0, top: 0 },
+  alignment = "horizontal",
+  siblings = 1,
+  index = -1,
+}: NodeComponentProps) => {
+  const { triggerOffset, setTriggerOffset } = useContext(NodeTreeContext);
+  const [open, setOpen] = useState<boolean>(defaultOpen);
+  const onClick = () => {
+    setOpen((prev) => !prev);
+    setTriggerOffset(Math.random());
+  };
+  const ref = useRef<HTMLDivElement>(null);
+  const timeout = useRef<number>(-1);
+  const [offset, setOffset] = useState<Offset>({
+    left: 0,
+    top: 0,
+  });
+  const color = useMemo(() => getRandomColor(), []);
 
-    const top = 100;
-    const width = childrenLength * 100;
-    const left = - (width / 2) + 50;
+  const getOffset = () => {
+    if (ref.current) {
+      const { left, top } = ref.current.getBoundingClientRect();
+      setOffset({ left, top });
+    }
+  };
 
-    return <>
-        <div className={css.nodeLevel} style={{ width, top, left }}>
-            {children}
+  useEffect(() => {
+    if (propagatedOpen !== null && !propagatedOpen) {
+      setOpen(false);
+      setTriggerOffset(Math.random());
+    }
+  }, [propagatedOpen]);
+
+  useEffect(() => {
+    getOffset();
+    window.addEventListener("resize", getOffset);
+    return () => window.removeEventListener("resize", getOffset);
+  }, [ref]);
+
+  useEffect(() => {
+    clearTimeout(timeout.current);
+    timeout.current = window.setTimeout(() => {
+      getOffset();
+    }, 500);
+    return () => {
+      clearTimeout(timeout.current);
+    };
+  }, [triggerOffset]);
+
+  const isVerticalMode = ForceVerticalMode.includes(name);
+
+  return (
+    <>
+      <div className={css.nodeContainer} style={style}>
+        {level !== 0 && alignment === "horizontal" && (
+          <>
+            <div
+              style={{
+                [index === 0 ? "right" : "left"]: 0,
+                ...((index as number) > 0 && index < siblings - 1
+                  ? { width: "100%" }
+                  : {}),
+              }}
+              className={css.nodeConnector}
+            ></div>
+          </>
+        )}
+        {level !== 0 && alignment === "vertical" && (
+          <>
+            {index === 0 && (
+              <div className={css.nodeFirstVerticalConnector}></div>
+            )}
+            <div
+              style={{
+                ...(index < siblings - 1 ? { height: "100%" } : {}),
+              }}
+              className={css.nodeConnectorVertical}
+            ></div>
+          </>
+        )}
+        {/* ------------------------------------------------------------ */}
+        <div
+          ref={ref}
+          className={`${css.node} ${css[alignment]}`}
+          onClick={onClick}
+        >
+          {level !== 0 && <div className={css.topNodeLine} />}
+          <div className={css.notification}>
+            <span>10</span>
+          </div>
+          <div className={css.body}>
+            <div className={css.flag} style={{ backgroundColor: color }} />
+            <span>
+              {name} {level}
+            </span>
+            <div className={css.grid} />
+          </div>
+          {nodes.length > 0 && open && <div className={css.bottomNodeLine} />}
         </div>
+        {/* ------------------------------------------------------------ */}
+        {nodes.length > 0 && (
+          <>
+            <Row
+              level={(level as number) + 1}
+              nodes={nodes}
+              open={open}
+              parentOffset={offset}
+              direction={isVerticalMode ? "vertical" : "horizontal"}
+            />
+          </>
+        )}
+      </div>
     </>
-}
+  );
+};
 
-const Node:FC<NodeProps> = ({ name, level, nodes }: NodeProps) => {
-    return <>
-        <div className={css.node}>
-            {name} 
-            { nodes.length > 0 && 
-                <NodeLevel level={level + 1}>
-                    {nodes?.map((node, index) => {
-                        return <Node data-level={level} level={level + 1} key={index} name={`${node.name} ${level + 1}`} nodes={node.nodes} />
-                    })}
-                </NodeLevel>
-            }
-        </div>
+const Row: FC<RowComponentProps> = ({
+  level,
+  nodes,
+  open,
+  parentOffset = { left: 0, top: 0 },
+  direction = "horizontal",
+}: RowComponentProps) => {
+  const sideValue = direction === "horizontal" ? nodeWidth : nodeHeight;
+  const property = direction === "horizontal" ? "marginLeft" : "marginTop";
+  const valueToOpen = `calc(-${nodes.length * sideValue}px - ${
+    nodes.length * 2
+  }vw - 0.3vw)`;
+
+  const style: CSSProperties = {
+    [property]: open ? 0 : valueToOpen,
+  };
+
+  const rowStyle: CSSProperties = {
+    opacity: open ? 1 : 0,
+    marginTop: open ? 0 : "-63px",
+  };
+  return (
+    <>
+      <div className={`${css.row} ${css[direction]}`} style={rowStyle}>
+        {nodes.map((node, index, array) => (
+          <Node
+            key={index}
+            index={index}
+            siblings={array.length}
+            style={index === nodes.length - 1 ? style : {}}
+            name={node.name}
+            level={level}
+            alignment={direction}
+            nodes={node.nodes}
+            propagatedOpen={open}
+            parentOffset={parentOffset}
+          />
+        ))}
+      </div>
     </>
-}
+  );
+};
 
 export default NodeTree;
--- src/pages/page_07.tsx
+++ src/pages/page_07.tsx
@@ -1,14 +1,14 @@
-import NodeTree from '../components/nodeTree';
-import css from '../styles/page.module.scss';
+import NodeTree from "../components/nodeTree";
+import css from "../styles/page.module.scss";
 
 const Page_03 = () => {
-
-  return <>
-    <div className={`${css.page}`}>
-      <NodeTree />
-    </div>
-  </>
-}
-
+  return (
+    <>
+      <div className={`${css.page} ${css.flex}`}>
+        <NodeTree />
+      </div>
+    </>
+  );
+};
 
 export default Page_03;
