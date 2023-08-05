6a6bbf2b72c042d4063c9e2b75951fd140dcdf98
Carlos1006 <cdgzz19@gmail.com>
2023-08-05 11:21:11 -0600

    Se prepara entorno para ruleta

---
+++ src/pages/page_09.tsx
@@ -0,0 +1,14 @@
+import css from "../styles/rouletteGraph.module.scss";
+
+const Page_09 = () => {
+
+  return (
+    <>
+      <div className={`${css.page}`} >
+       
+      </div>
+    </>
+  );
+};
+
+export default Page_09;
