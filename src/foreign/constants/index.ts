802ad9292c37abe86c5024ab5aa9cbbeaffffdff
Carlos1006 <cdgzz19@gmail.com>
2023-09-16 14:41:13 -0600

    Update

--- src/foreign/constants/index.ts
+++
@@ -1,6 +0,0 @@
-export const { sin: SINE, cos: COSINE, atan2: ARCTAN2, PI } = Math;
-export const VIEW = 360;
-export const HALF_VIEW = VIEW / 2;
-export const R2D = 180 / PI;
-export const TRIANGLE_SIDE = 25;
-export const TRIANGLE_HALF_SIDE = TRIANGLE_SIDE / 2;
