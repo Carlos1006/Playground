d17651bcbebfb2e5e1dffc285d67139a3e218417
Carlos1006 <cdgzz19@gmail.com>
2023-10-26 10:06:38 -0600

    Se comienza web2

---
+++ src/website/web_02/constants/index.ts
@@ -0,0 +1,45 @@
+import imgCbp00 from "../assets/cyberpunk_00.png";
+import imgCbp01 from "../assets/cyberpunk_01.png";
+import imgCbp02 from "../assets/cyberpunk_02.png";
+import imgCbp03 from "../assets/cyberpunk_03.png";
+import imgCbp04 from "../assets/cyberpunk_04.png";
+import imgCbp05 from "../assets/cyberpunk_05.png";
+import imgCbp06 from "../assets/cyberpunk_06.png";
+import imgCbp07 from "../assets/cyberpunk_07.png";
+import imgCbp08 from "../assets/cyberpunk_08.png";
+import imgCbp09 from "../assets/cyberpunk_09.png";
+import imgCbp10 from "../assets/cyberpunk_10.png";
+import imgCbp11 from "../assets/cyberpunk_11.png";
+import imgCbp12 from "../assets/cyberpunk_12.png";
+import imgCbp13 from "../assets/cyberpunk_13.png";
+import imgCbp14 from "../assets/cyberpunk_14.png";
+import imgCbp15 from "../assets/cyberpunk_15.png";
+import imgCbp16 from "../assets/cyberpunk_16.png";
+import imgCbp17 from "../assets/cyberpunk_17.png";
+import imgCbp18 from "../assets/cyberpunk_18.png";
+import imgCbp19 from "../assets/cyberpunk_19.png";
+import { IProfile } from "../types";
+
+export const PROFILES: IProfile[]  = [
+ { image:imgCbp00,name:"Spike Spiegel"},
+ { image:imgCbp01,name:"Jet Black"},
+ { image:imgCbp02,name:"Faye Valentine"},
+ { image:imgCbp03,name:"Edward Wong"},
+ { image:imgCbp04,name:"Radical Edward"},
+ { image:imgCbp05,name:"Hau Pepelu"},
+ { image:imgCbp06,name:"Tivrusky IV"},
+ { image:imgCbp07,name:"Ein"},
+ { image:imgCbp08,name:"Vicious"},
+ { image:imgCbp09,name:"Julia"},
+ { image:imgCbp10,name:"Twinkle Maria"},
+ { image:imgCbp11,name:"Murdock"},
+ { image:imgCbp12,name:"Annie"},
+ { image:imgCbp13,name:"Punch"},
+ { image:imgCbp14,name:"Judy"},
+ { image:imgCbp15,name:"Andy Von de Oniyate"},
+ { image:imgCbp16,name:"Chessmaster Hex"},
+ { image:imgCbp17,name:"Mad Pierrot"},
+ { image:imgCbp18,name:"Tongpu"},
+ { image:imgCbp19,name:"Lin"}
+].reverse();
+
--- src/website/web_02/index.tsx
+++ src/website/web_02/index.tsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React from "react";
+import css from "./style/main.module.scss";
+import { PROFILES } from "./constants";
+import { IProfile } from "./types";
 
 const Web_02: React.FC = () => {
-    return (
-        <div>
-            <h1>Welcome to Web_02 component!</h1>
+  return (
+    <div id={css.main}>
+      <div id={css.roof}></div>
+      <div id={css.visorContainer}>
+        <div id={css.navLeft} className={css.navigator}></div>
+        <div id={css.navRight} className={css.navigator}></div>
+        <div id={css.visor}>
+          {PROFILES.reverse().map((profile: IProfile, index: number) => {
+            const { image } = profile;
+            return (
+              <div key={index} className={css.card}>
+                <img src={image} />
+              </div>
+            );
+          })}
         </div>
-    );
+      </div>
+      <div id={css.spinner}></div>
+    </div>
+  );
 };
 
 export default Web_02;
