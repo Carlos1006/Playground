e09ebd30e36db8960324104e4b0793fcd8d5f737
Naker21 <cdgzz19@gmail.com>
2024-08-15 00:14:57 -0600

    style(web15): se crea estructura del bento grid

--- src/website/web_15/index.tsx
+++ src/website/web_15/index.tsx
@@ -6,19 +6,65 @@ const BentoGrid: FC = () => {
     <div id={css.main}>
       <div id={css.wrapper}>
         <div className={css.header} />
-        <div className={css.element00}></div>
-        <div className={css.element01}></div>
-        <div className={css.element02}></div>
-        <div className={css.element10}></div>
-        <div className={css.element11}></div>
-        <div className={css.element12}></div>
-        <div className={css.element13}></div>
-        <div className={css.element20}></div>
-        <div className={css.element30}></div>
-        <div className={css.element40}></div>
-        <div className={css.element41}></div>
-        <div className={css.element42}></div>
-        <div className={css.footer} />
+        <div className={css.element00}>
+          <div className={css.wrapper01}>
+            <div className={css.wrapper02}></div>
+          </div>
+        </div>
+        <div className={css.element01}>
+          <div className={css.wrapper}></div>
+        </div>
+        <div className={css.element02}>
+          <div className={css.wrapper01}>
+            <div className={css.wrapper02}></div>
+          </div>
+        </div>
+        <div className={css.element10}>
+          <div className={css.wrapper01}>
+            <div className={css.wrapper02}></div>
+          </div>
+        </div>
+        <div className={css.element11}>
+          <div className={css.overflowWrapper01}>
+            <div className={css.overflowWrapper02}></div>
+          </div>
+          <div className={css.wrapper01}>
+            <div className={css.wrapper02}></div>
+          </div>
+        </div>
+        <div className={css.element12}>
+          <div className={css.wrapper01}></div>
+        </div>
+        <div className={css.element13}>
+          <div className={css.wrapper01}></div>
+        </div>
+        <div className={css.element20}>
+          <div className={css.wrapper01}>
+            <div className={css.wrapper02}></div>
+          </div>
+        </div>
+        <div className={css.element30}>
+          <div className={css.wrapper01}>
+            <div className={css.wrapper02}></div>
+          </div>
+        </div>
+        <div className={css.element40}>
+          <div className={css.wrapper01}>
+            <div className={css.wrapper02}></div>
+          </div>
+        </div>
+        <div className={css.element41}>
+          <div className={css.wrapper01}>
+            <div className={css.wrapper02}></div>
+          </div>
+        </div>
+        <div className={css.element42}>
+          <div className={css.wrapper01}></div>
+        </div>
+        <div className={css.footer}>
+          <div className={css.overflowWrapper} />
+          <div className={css.wrapper}></div>
+        </div>
         <div className={css.gap} />
       </div>
     </div>
