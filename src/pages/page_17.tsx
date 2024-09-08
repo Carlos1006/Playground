cf979baf32488d3ca8bf1abce2f1bbbcfb6edc4c
Naker21 <cdgzz19@gmail.com>
2024-09-08 02:49:29 -0600

    feat(page_17): Add new page component for page_17 to test jquery 4

---
+++ src/pages/page_17.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+import css from "../styles/page.module.scss";
+// jQuery 4 Testing
+
+const Page_17: FC = () => {
+  return (
+    <>
+      <div className={`${css.page} ${css.black} ${css.relative}`}></div>
+    </>
+  );
+};
+
+export default Page_17;
