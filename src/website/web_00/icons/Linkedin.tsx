1cdad25754671033e3c72e5ffb65732ec8888b51
Naker21 <cdgzz19@gmail.com>
2024-10-06 08:42:43 -0600

    feat(web_00): add new social media icons (Email, Instagram, Pinterest, Linkedin) and assets

---
+++ src/website/web_00/icons/Linkedin.tsx
@@ -0,0 +1,25 @@
+import { ReactNode } from "react";
+
+export function MdiLinkedin(props: HTMLOrSVGElement): ReactNode {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" {...props}>
+      <path
+        fill="currentColor"
+        d="M19 3a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2zm-.5 15.5v-5.3a3.26 3.26 0 0 0-3.26-3.26c-.85 0-1.84.52-2.32 1.3v-1.11h-2.79v8.37h2.79v-4.93c0-.77.62-1.4 1.39-1.4a1.4 1.4 0 0 1 1.4 1.4v4.93zM6.88 8.56a1.68 1.68 0 0 0 1.68-1.68c0-.93-.75-1.69-1.68-1.69a1.69 1.69 0 0 0-1.69 1.69c0 .93.76 1.68 1.69 1.68m1.39 9.94v-8.37H5.5v8.37z"
+      ></path>
+    </svg>
+  );
+}
+
+export default function IcomoonFreeLinkedin2(
+  props: HTMLOrSVGElement
+): ReactNode {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" {...props}>
+      <path
+        fill="currentColor"
+        d="M6 6h2.767v1.418h.04C9.192 6.727 10.134 6 11.539 6C14.46 6 15 7.818 15 10.183V15h-2.885v-4.27c0-1.018-.021-2.329-1.5-2.329c-1.502 0-1.732 1.109-1.732 2.255V15H6zM1 6h3v9H1zm3-2.5a1.5 1.5 0 1 1-3.001-.001A1.5 1.5 0 0 1 4 3.5"
+      ></path>
+    </svg>
+  );
+}
