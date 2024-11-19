0d80f22319573058972dca480b200afed1048521
Carlos Gonzalez <cdgzz19@gmail.com>
2024-11-18 23:01:31 -0600

    feat-wip(web00): improving pixelated component dragging behavior

--- src/components/pixelate/index.tsx
+++ src/components/pixelate/index.tsx
@@ -1,17 +1,24 @@
 import { FC, useEffect, useRef, useState } from "react";
 import css from "./styles/main.module.scss";
 import octopusImage from "./assets/772154_an_octopus_made_of_glass_floating_in_cyan_and_purp_xl-1024-v1-0.png";
+import animeImage from "./assets/226981_an_anime_girl_with_black_and_pink_hair_and_headpho_xl-1024-v1-0.png";
 import { createPixelMatrix } from "./utils";
 import Mask from "./model/Mask";
 import DragIcon from "./icons/DragIcon";
 const pixelRadius = 10;
 const pixelSize = pixelRadius * 2 + 1;
 
+const IMG = octopusImage;
+
 const Pixelate: FC = () => {
   const [position, setPosition] = useState(50);
   const [isDragging, setIsDragging] = useState(false);
   const draggerContainer = useRef<HTMLDivElement>(null);
   const canvasRef = useRef<HTMLCanvasElement>(null);
+  const imgRef = useRef<HTMLImageElement>(null);
+  const [draggerHeight, setDraggerHeight] = useState(0);
+  const [fullHeight, setFullHeight] = useState(false);
+  const [draggerWidth, setDraggerWidth] = useState(0);
 
   useEffect(() => {
     const canvas = canvasRef.current;
@@ -20,11 +27,14 @@ const Pixelate: FC = () => {
       if (!canvas) return;
       const ctx = canvas.getContext("2d");
       const img = new Image();
-      img.src = octopusImage;
+      img.src = IMG;
       await new Promise((resolve) => {
         img.onload = (): void => resolve(true);
       });
       if (!ctx) return;
+
+      setFullHeight(img.height >= img.width);
+
       canvas.width = img.width;
       canvas.height = img.height;
       ctx.drawImage(img, 0, 0);
@@ -53,6 +63,22 @@ const Pixelate: FC = () => {
     };
   }, []);
 
+  useEffect(() => {
+    if (imgRef.current) {
+      const resizeObserver = new ResizeObserver((entries) => {
+        for (const entry of entries) {
+          const { height, width } = entry.contentRect;
+          setDraggerHeight(height);
+          setDraggerWidth(width);
+        }
+      });
+      resizeObserver.observe(imgRef.current);
+      return () => {
+        resizeObserver.disconnect();
+      };
+    }
+  }, []);
+
   const onMouseMove = (e: MouseEvent): void => {
     if (!isDragging) return;
     if (!draggerContainer.current) return;
@@ -71,11 +97,15 @@ const Pixelate: FC = () => {
 
   return (
     <div id={css.pixelate}>
-      <div id={css.container}>
+      <div id={css.container} className={`${fullHeight ? css.fullHeight : ""}`}>
         <div
           id={css.draggerContainer}
           ref={draggerContainer}
           className={`${isDragging ? css.dragging : ""}`}
+          style={{
+            height: `${draggerHeight}px`,
+            width: `${draggerWidth}px`,
+          }}
           onMouseLeave={(): void => setIsDragging(false)}
           onMouseMove={(e): void => onMouseMove(e.nativeEvent)}
           onMouseDown={(e): void => onMouseDown(e.nativeEvent)}
@@ -87,7 +117,7 @@ const Pixelate: FC = () => {
             </div>
           </div>
         </div>
-        <img src={octopusImage} alt="Octopus" />
+        <img src={IMG} alt="Octopus" ref={imgRef} />
         <canvas id={css.canvas} ref={canvasRef} style={{ clipPath }} />
         <div id={css.shadow} />
       </div>
